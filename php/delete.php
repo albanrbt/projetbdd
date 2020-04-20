9aa70a1a79976297e133e87d05dd558b63493c73
ALLEZLABANANE <alban.rambert@gmail.com>
2020-04-20 09:54:28 +0200

    ajout erreur

--- php/delete.php
+++ php/delete.php
@@ -14,5 +14,6 @@ $Sql2 = "DELETE FROM users  WHERE Id_User =".$ID;
 if(mysqli_query($con, $Sql1)){
     if(mysqli_query($con, $Sql2)){
 echo "<script> alert('enregistré')</script>";}}
+else{echo "<script> alert('erreur')</script>";}
 
 echo "<script>window.history.go(-1);</script>";
